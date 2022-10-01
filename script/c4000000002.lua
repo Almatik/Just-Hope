e39e6e8e8f111d81f46f9c7bd79e84330ec2701b
Almatik <almatiktk@gmail.com>
2022-10-02 03:40:31 +0600

    f

---
+++ script/c4000000002.lua
@@ -0,0 +1,4 @@
+--Hanasaka Shoujo
+function c4000000002.initial_effect(c)
+	
+end
---
+++ script/c4000000004.lua
@@ -0,0 +1,91 @@
+--Starliege Photon Paladin
+local s,id=GetID()
+function s.initial_effect(c)
+	--Xyz Summon
+	c:EnableReviveLimit()
+	Xyz.AddProcedure(c,nil,8,2,s.ovfilter,aux.Stringid(id,0))
+	--Xyz Limit
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--Search or send to GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_TOGRAVE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,2})
+	e2:SetCost(aux.dxmcostgen(1,1,nil))
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+	--destroy replace
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetRange(LOCATION_GRAVE+LOCATION_MZONE)
+	e3:SetCountLimit(1,{id,3})
+	e3:SetTarget(s.reptg)
+	e3:SetValue(s.repval)
+	e3:SetOperation(s.repop)
+	c:RegisterEffect(e3)
+end
+function s.filter(c)
+	return c:IsFaceup and c:IsSetCard(0x107b)
+end
+function s.ovfilter(c,tp,lc)
+	if Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil) then
+		return c:IsFaceup() and c:IsType(TYPE_XYZ,lc,SUMMON_TYPE_XYZ,tp)
+	else
+		false
+	end
+end
+
+
+
+
+
+
+
+function s.takefilter(c)
+	return (c:IsSetCard(0x55) or c:IsSetCard(0x7b))and (c:IsAbleToHand() or c:IsAbleToGrave())
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.takefilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.takefilter,tp,LOCATION_DECK,0,1,1,nil)
+	aux.ToHandOrElse(g:GetFirst(),tp)
+end
+
+
+
+
+
+
+
+
+
+function s.repfilter(c,tp)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ) 
+		and (c:IsSetCard(0x55) or c:IsSetCard(0x7b))
+		and c:GetOriginalAttribute() & ATTRIBUTE_LIGHT ~= 0 and not c:IsCode(id) 
+		and c:IsLocation(LOCATION_MZONE) and c:IsControler(tp) 
+		and not c:IsReason(REASON_REPLACE) and c:IsReason(REASON_EFFECT+REASON_BATTLE)
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToRemove() and not c:IsStatus(STATUS_DESTROY_CONFIRMED)
+		and eg:IsExists(s.repfilter,1,nil,tp) end
+	return Duel.SelectEffectYesNo(tp,c,96)
+end
+function s.repval(e,c)
+	return s.repfilter(c,e:GetHandlerPlayer())
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
+end
