fbb93afc673b773bb43a37204558e4a097d6df69
Almatik <almatiktk@gmail.com>
2022-10-05 22:15:29 +0600

    Bleach

--- script/c3900010125.lua
+++ script/c3900010125.lua
@@ -1,22 +1,11 @@
---Karakura Shinigami - Ichigo
+--Karakura Quincy - Ishida
 local s,id=GetID()
 function s.initial_effect(c)
 	--link summon
 	Link.AddProcedure(c,nil,2,2,s.lcheck)
 	c:EnableReviveLimit()
 	--Link Summoned
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.lscon)
-	e1:SetCost(s.lscost)
-	e1:SetTarget(s.lstg)
-	e1:SetOperation(s.lsop)
-	c:RegisterEffect(e1)
-	aux.AddEREquipLimit(c,nil,s.eqval,s.equipop,e1)
+
 	--Special Summon this card
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
@@ -31,38 +20,14 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 s.listed_names={id}
-s.listed_series={0x39a1,0x39a2}
+s.listed_series={0x39a1,0x39a9}
 function s.lcheck(g,lc,sumtype,tp)
 	return g:IsExists(Card.IsSetCard,1,nil,0x39a1,lc,sumtype,tp)
 		and g:GetClassCount(Card.GetCode)==#g
 end
 
 --Link Summoned
-function s.eqval(ec,c,tp)
-	return ec:IsControler(tp) and ec:IsSetCard(0x39a4)
-end
-function s.equipop(c,e,tp,tc)
-	c:EquipByEffectAndLimitRegister(e,tp,tc,nil,true)
-end
-function s.lsfilter(c)
-	return c:IsSetCard(0x39a4) and c:IsMonster() and not c:IsForbidden()
-end
-function s.lstg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingMatchingCard(s.lsfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
-end
-function s.lsop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
-	if c:IsFacedown() or not c:IsRelateToEffect(e) then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.lsfilter),tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil)
-	local tc=g:GetFirst()
-	if tc then
-		s.equipop(c,e,tp,tc)
-	end
-end
+
 
 --Special Summon from GY
 function s.spfilter(c,e,tp,sc)
