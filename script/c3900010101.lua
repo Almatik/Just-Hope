2c72c79ac14221209bfbe57890e9397ad4ab7969
Almatik <almatiktk@gmail.com>
2022-10-05 21:25:54 +0600

    Update c3900010101.lua

--- script/c3900010101.lua
+++ script/c3900010101.lua
@@ -1,5 +1,94 @@
 --Karakura Student - Ichigo
 local s,id=GetID()
 function s.initial_effect(c)
-	
+	--Extra Material
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCode(EFFECT_EXTRA_MATERIAL)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetOperation(s.extracon)
+	e1:SetValue(s.extraval)
+	c:RegisterEffect(e1)
+	if s.flagmap==nil then
+		s.flagmap={}
+	end
+	if s.flagmap[c]==nil then
+		s.flagmap[c] = {}
+	end
+	--Material
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_REMOVE+CATEGORY_DRAW+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_BE_MATERIAL)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.matcon)
+	e1:SetTarget(s.mattg)
+	e1:SetOperation(s.matop)
+	c:RegisterEffect(e1)
+end
+s.listed_names={id,BLEACH_ICHIGO}
+s.listed_series={0x39a1}
+
+--Additional Material
+function s.extrafilter(c,tp)
+	return c:IsLocation(LOCATION_MZONE) and c:IsControler(tp)
+end
+function s.extracon(c,e,tp,sg,mg,lc,og,chk)
+	return (sg+mg):Filter(s.extrafilter,nil,e:GetHandlerPlayer()):IsExists(Card.IsFaceup,1,og,nil) and sg:FilterCount(s.flagcheck,nil)<2
+end
+function s.flagcheck(c)
+	return c:GetFlagEffect(id)>0
+end
+function s.extraval(chk,summon_type,e,...)
+	local c=e:GetHandler()
+	if chk==0 then
+		local tp,sc=...
+		if summon_type~=SUMMON_TYPE_LINK or not sc:IsSetCard(0x39a1) or Duel.GetFlagEffect(tp,id)>0 then
+			return Group.CreateGroup()
+		else
+			table.insert(s.flagmap[c],c:RegisterFlagEffect(id,0,0,1))
+			return Group.FromCards(c)
+		end
+	elseif chk==1 then
+		local sg,sc,tp=...
+		if summon_type&SUMMON_TYPE_LINK == SUMMON_TYPE_LINK and #sg>0 then
+			Duel.Hint(HINT_CARD,tp,id)
+			Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+		end
+	elseif chk==2 then
+		for _,eff in ipairs(s.flagmap[c]) do
+			eff:Reset()
+		end
+		s.flagmap[c]={}
+	end
+end
+
+--When used as material
+function s.matcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	return c:IsLocation(LOCATION_GRAVE)
+		and (c:IsPreviousLocation(LOCATION_ONFIELD) or c:IsPreviousLocation(LOCATION_HAND))
+		and rc:IsSetCard(0x39a1) and r&REASON_LINK~=0
+end
+function s.matfilter(c)
+	return c:IsSetCard(0x39a1) and c:IsType(TYPE_MONSTER)
+		and c:IsAbleToHand() and not c:IsCode(id)
+end
+function s.mattg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.matfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.matop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.matfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,tp,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
