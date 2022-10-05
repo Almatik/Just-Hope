353e97c5a1bbe8b928a26afcb402461c6c1332be
Almatik <almatiktk@gmail.com>
2022-10-05 21:52:14 +0600

    Update c3900010103.lua

--- script/c3900010103.lua
+++ script/c3900010103.lua
@@ -82,7 +82,7 @@ function s.matfilter(c,e,tp)
 end
 function s.mattg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-			and Duel.IsExistingMatchingCard(s.matfilter,tp,LOCATION_HAND,0,1,nil) end
+		and Duel.IsExistingMatchingCard(s.matfilter,tp,LOCATION_HAND,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function s.matop(e,tp,eg,ep,ev,re,r,rp)
