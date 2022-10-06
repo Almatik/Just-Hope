a6d14434ddcbcaf2d513ff1963a157b248a05c89
JustGarnet <70763640+JustGarnet@users.noreply.github.com>
2022-10-07 00:03:55 +0300

    SS link

---
+++ script/c3800006603.lua
@@ -0,0 +1,57 @@
+--Sea Sharp- Sisters
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special summon when Link Summoned
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(0,{id,1}))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.spcon1)
+	e1:SetTarget(s.sptg1)
+	e1:SetOperation(s.spop1)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x38a1}
+
+function s.spcon1(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+function s.spfilter1(c,e,tp)
+	return c:IsSetCard(0x38a1) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP) and c::IsLevelBelow(2)
+end
+function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_DECK) and chkc:IsControler(tp) and s.spfilter1(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.spfilter1,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.spfilter1,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,LOCATION_DECK)
+	if c:IsRelateToEffect(e) then
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_FIELD)
+		e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e2:SetCode(EFFECT_CANNOT_ACTIVATE)
+		e2:SetTargetRange(1,0)
+		e2:SetTarget(s.splimit)
+		e2:SetValue(1)
+		e2:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e2,tp)
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_FIELD)
+		e3:SetCode(EFFECT_DISABLE)
+		e3:SetTargetRange(LOCATION_ONFIELD,0)
+		e3:SetTarget(s.splimit)
+		e3:SetTarget(s.disable)
+		e3:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e3,tp)
+	end
+	function s.splimit(e,c)
+	return not c:IsSetCard(0x38a1)
+	end
+	function s.disable(e,c)
+	return c~=e:GetHandler() and (not c:IsMonster() or (c:IsType(TYPE_EFFECT) or (c:GetOriginalType()&TYPE_EFFECT)==TYPE_EFFECT)) and not c:IsSetCard(0x38a1)
+	end
+end
+end
