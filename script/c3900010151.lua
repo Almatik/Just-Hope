cbeb79e7d9c0c96b2b7e1044211be407357d179b
Almatik <almatiktk@gmail.com>
2022-10-07 19:31:06 +0600

    Update c3900010151.lua

--- script/c3900010151.lua
+++ script/c3900010151.lua
@@ -18,18 +18,20 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.thfilter(c,e,tp)
 	return c:IsSetCard(0x39a1) and c:IsMonster()
-	and (c:IsAbleToHand() and c:IsLocation(LOCATION_DECK))
-	or (c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsLocation(LOCATION_HAND))
+		and ((c:IsAbleToHand() and c:IsLocation(LOCATION_DECK))
+		or (c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsLocation(LOCATION_HAND)))
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
-	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) and g:IsAbleToHand() then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-	elseif g:IsCanBeSpecialSummoned(e,0,tp,false,false) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	if #g>0 then
+		if g:IsLocation(LOCATION_DECK) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		elseif g:IsLocation(LOCATION_HAND) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
 	end
 end
