387a1bcf8d60fdba3ed67846a77d5cd18dbde6bb
Almatik <almatiktk@gmail.com>
2022-10-07 20:30:27 +0600

    Update c3900010154.lua

--- script/c3900010154.lua
+++ script/c3900010154.lua
@@ -3,50 +3,82 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND+CATEGORY_TODECK)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.activate1)
 	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.activate2)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+	e3:SetTarget(s.target3)
+	e3:SetOperation(s.activate3)
+	c:RegisterEffect(e3)
 end
 s.listed_series={0x39a1}
 function s.filter1(c,e,tp)
 	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x39a1)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
 end
-function s.filter2(c)
-	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x39a1)
-		and c:IsAbleToHand()
-end
-function s.filter3(c)
-	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x39a1)
-		and c:IsAbletoDeck()
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ct
-	local g
-	local g1=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_GRAVE,0,nil,e,tp)
-	local g2=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_GRAVE,0,nil)
-	local g3=Duel.GetMatchingGroup(s.filter3,tp,LOCATION_GRAVE,0,nil)
-	if g:GetClassCount(Card.GetCode)>=1 then ct=1 and g:Merge(g1) end
-	if g:GetClassCount(Card.GetCode)>=2 then ct=2 and g:Merge(g2) end
-	if g:GetClassCount(Card.GetCode)>=3 then ct=3 and g:Merge(g3) end
-	if chk==0 then return g:GetClassCount(Card.GetCode)>=ct end
-	local tg=aux.SelectUnselectGroup(g,e,tp,1,ct,aux.dncheck,1,tp,HINTMSG_SPSUMMON)
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_GRAVE,0,nil,e,tp)
+	if chk==0 then return g:GetClassCount(Card.GetCode)>=1 end
+	local tg=aux.SelectUnselectGroup(g,e,tp,1,1,aux.dncheck,1,tp,HINTMSG_SPSUMMON)
 	Duel.SetTargetCard(tg)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,tg,#tg,0,0)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.activate1(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
 	local sg=g:Filter(Card.IsRelateToEffect,nil,e)
 	if #sg==1 then
 		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
-	elseif #sg==2 then
+	end
+end
+
+function s.filter2(c)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x39a1)
+		and c:IsAbleToHand()
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_GRAVE,0,nil)
+	if chk==0 then return g:GetClassCount(Card.GetCode)>=2 end
+	local tg=aux.SelectUnselectGroup(g,e,tp,2,2,aux.dncheck,1,tp,HINTMSG_SPSUMMON)
+	Duel.SetTargetCard(tg)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,tg,#tg,0,0)
+end
+function s.activate2(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local sg=g:Filter(Card.IsRelateToEffect,nil,e)
+	if #sg==2 then
 		Duel.SendtoHand(sg,nil,REASON_EFFECT)
-	elseif #sg==3 then
+	end
+end
+
+function s.filter3(c)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x39a1)
+		and c:IsAbleToDeck()
+end
+function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.filter3,tp,LOCATION_GRAVE,0,nil)
+	if chk==0 then return g:GetClassCount(Card.GetCode)>=3 end
+	local tg=aux.SelectUnselectGroup(g,e,tp,3,3,aux.dncheck,1,tp,HINTMSG_SPSUMMON)
+	Duel.SetTargetCard(tg)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,tg,#tg,0,0)
+end
+function s.activate3(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local sg=g:Filter(Card.IsRelateToEffect,nil,e)
+	if #sg==3 then
 		Duel.SendtoDeck(sg,tp,2,REASON_EFFECT)
 		Duel.Draw(tp,1,REASON_EFFECT)
 	end
