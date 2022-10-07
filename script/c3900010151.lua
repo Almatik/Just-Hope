4bb8a30e5dd5b7950154766ce324fe8c8679a3e5
Almatik <almatiktk@gmail.com>
2022-10-07 19:41:14 +0600

    f

--- script/c3900010151.lua
+++ script/c3900010151.lua
@@ -9,6 +9,19 @@ function s.initial_effect(c)
 	e0:SetTarget(s.target)
 	e0:SetOperation(s.activate)
 	c:RegisterEffect(e0)
+	--Karakura
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetRange(LOCATION_FZONE)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.effcon)
+	e1:SetTarget(s.tg1)
+	e1:SetOperation(s.op1)
+	e1:SetLabel(0x39a1)
+	c:RegisterEffect(e1)
 end
 s.listed_names={0x39a1}
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -33,5 +46,31 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		end
+	end
+end
+
+
+
+
+function s.confilter(c,label)
+	return c:IsFaceup() and c:IsSetCard(label) and c:IsControler(tp) and c:IsSummonLocation(LOCATION_EXTRA)
+end
+function s.effcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.confilter,1,nil,e:GetLabel())
+end
+function s.tg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+function s.op1(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	if Duel.Draw(p,d,REASON_EFFECT)<2 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,1,nil)
+	if #g>0 then
+		Duel.BreakEffect()
+		Duel.SendtoDeck(g,nil,0,REASON_EFFECT)
 	end
 end
