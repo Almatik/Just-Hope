34c6a97ee2db4c4e94f7ca90c3d95781b346f5ec
Almatik <almatiktk@gmail.com>
2022-10-08 12:36:30 +0600

    Update c3800006603.lua

--- script/c3800006603.lua
+++ script/c3800006603.lua
@@ -6,7 +6,7 @@ function s.initial_effect(c)
 	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x38a1),2,2)
 	--Special summon when Link Summoned
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(0,{id,1}))
+	e1:SetDescription(aux.Stringid(0,1))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
