a9d9796d70f1d9ef4477095f98c8b2516bfada9f
Almatik <almatiktk@gmail.com>
2022-10-09 16:09:51 +0600

    Update c3900010152.lua

--- script/c3900010152.lua
+++ script/c3900010152.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Special summon 1 level 5 or lower fusion monster from extra deck
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_LINK_SUMMON)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
