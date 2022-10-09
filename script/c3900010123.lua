c69ec6570e2b90f72e6c4c5ceffe031a76bafaf5
Almatik <almatiktk@gmail.com>
2022-10-09 18:12:24 +0600

    Update c3900010123.lua

--- script/c3900010123.lua
+++ script/c3900010123.lua
@@ -5,7 +5,15 @@ function s.initial_effect(c)
 	Link.AddProcedure(c,nil,2,2,s.lcheck)
 	c:EnableReviveLimit()
 	--Link Summoned
-
+	--indes
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(s.indtg)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
 	--Special Summon this card
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
@@ -28,6 +36,10 @@ end
 
 --Link Summoned
 
+--Indes
+function s.indtg(e,c)
+	return c:IsLinked()
+end
 
 --Special Summon from GY
 function s.spfilter(c,e,tp,sc)
