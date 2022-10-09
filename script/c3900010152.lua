7b5df3d68cdca987b41d15cecabbcb22785f0cf6
Almatik <almatiktk@gmail.com>
2022-10-09 16:08:25 +0600

    Update c3900010152.lua

--- script/c3900010152.lua
+++ script/c3900010152.lua
@@ -68,5 +68,5 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.sfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,lv)
 	local tc=g:GetFirst()
-	if tc then Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP) end
+	if tc then Duel.SpecialSummon(tc,SUMMON_TYPE_LINK,tp,tp,false,false,POS_FACEUP) end
 end
