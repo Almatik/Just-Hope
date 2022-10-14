7efe42a516c5da893bc22400d812e59480107183
Almatik <almatiktk@gmail.com>
2022-10-15 00:55:24 +0600

    f

--- script/c3900020002.lua
+++ script/c3900020002.lua
@@ -18,11 +18,33 @@ function s.initial_effect(c)
 	pe2:SetType(EFFECT_TYPE_IGNITION)
 	pe2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	pe2:SetRange(LOCATION_PZONE)
-	pe2:SetCountLimit(1)
+	pe2:SetCountLimit(1,{id,2})
 	pe2:SetCost(s.pencost)
 	pe2:SetTarget(s.pentg)
 	pe2:SetOperation(s.penop)
 	c:RegisterEffect(pe2)
+	--special summon
+	local me1=Effect.CreateEffect(c)
+	me1:SetType(EFFECT_TYPE_FIELD)
+	me1:SetCode(EFFECT_SPSUMMON_PROC)
+	me1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	me1:SetRange(LOCATION_HAND)
+	me1:SetCountLimit(1,{id,3})
+	me1:SetCondition(s.spcon)
+	me1:SetTarget(s.sptg)
+	me1:SetOperation(s.spop)
+	c:RegisterEffect(me1)
+	--Search or send to the GY 1 "Tearalaments" card from your Deck
+	local me2=Effect.CreateEffect(c)
+	me2:SetDescription(aux.Stringid(id,1))
+	me2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_TOGRAVE)
+	me2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	me2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	me2:SetProperty(EFFECT_FLAG_DELAY)
+	me2:SetCountLimit(1,{id,4})
+	me2:SetTarget(s.thtg)
+	me2:SetOperation(s.thop)
+	c:RegisterEffect(me2)
 end
 function s.splimit(e,c,sump,sumtype,sumpos,targetp)
 	if c:IsSetCard(0x9e) then return false end
@@ -68,5 +90,50 @@ function s.penop(e,tp,eg,ep,ev,re,r,rp)
 		e4:SetCode(EFFECT_CHANGE_RSCALE)
 		e4:SetValue(tc:GetRightScale()-c:GetLeftScale())
 		tc:RegisterEffect(e4)
+	end
+end
+
+
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local rg=Duel.GetMatchingGroup(Card.IsDiscardable,tp,LOCATION_HAND,0,e:GetHandler())
+	return aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),0,c)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+	local rg=Duel.GetMatchingGroup(Card.IsDiscardable,tp,LOCATION_HAND,0,e:GetHandler())
+	local g=aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),1,tp,HINTMSG_DISCARD,nil,nil,true)
+	if #g>0 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.SendtoGrave(g,REASON_DISCARD+REASON_COST)
+	g:DeleteGroup()
+end
+
+
+function s.thfilter(c)
+	return c:IsLevelBelow(5) and c:IsSetCard(0x8e) and c:IsType(TYPE_MONSTER)
+		and (c:IsType(TYPE_PENDULUN)
+			or (not c:IsType(TYPE_PENDULUM) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false))))
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc:IsType(TYPE_PENDULUM) then
+		Duel.MoveToField(tc,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	else
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
