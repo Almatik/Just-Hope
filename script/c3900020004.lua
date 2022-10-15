150dc9bcc36d1abce3f7416bd3be5ef59a36f3e8
Almatik <almatiktk@gmail.com>
2022-10-15 22:54:47 +0600

    Update c3900020004.lua

--- script/c3900020004.lua
+++ script/c3900020004.lua
@@ -2,14 +2,24 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--skill
-	aux.AddVrainsSkillProcedure(c,s.flipcon,s.flipop)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFlagEffect(tp,id)==0
+	return aux.CanActivateSkill(tp)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.RegisterFlagEffect(tp,id,0,0,RESET_PHASE+PHASE_END)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
 	Duel.TagSwap(tp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetCountLimit(1)
+	e1:SetOperation(s.EPop)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.EPop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
 end
