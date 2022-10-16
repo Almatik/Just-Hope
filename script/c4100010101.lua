d478ce549875e9222b1101a89bb7441455094f21
Almatik <almatiktk@gmail.com>
2022-10-16 22:23:33 +0600

    Update c4100010101.lua

--- script/c4100010101.lua
+++ script/c4100010101.lua
@@ -6,8 +6,8 @@ function s.initial_effect(c)
 	DuelLinks.StartUp(c,nil,s.flipop)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SKILL_FLIP,e:GetHandlerPlayer(),id|(1<<32))
-	Duel.Hint(HINT_CARD,e:GetHandlerPlayer(),id)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
 	local tc=Duel.CreateToken(tp,24874630)
 	Duel.SendtoDeck(tc,tp,2,REASON_RULE)
 end
