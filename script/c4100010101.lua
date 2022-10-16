c70c41d8e8599e896d1e05ceaf310c9475d59d2b
Almatik <almatiktk@gmail.com>
2022-10-16 21:16:35 +0600

    f

--- script/c4100010101.lua
+++ script/c4100010101.lua
@@ -2,7 +2,8 @@
 Duel.LoadScript("duellinks.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	DuelLinks.Procedure(c)
+	--DuelLinks.AddProcedure(c)
+	DuelLinks.Predraw(c,flipcon,flipop,1)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnCount()==1
