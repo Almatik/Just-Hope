bb8939d0f2fdb37cb02ca34199d16f4ec6a81292
Almatik <almatiktk@gmail.com>
2022-10-16 21:53:35 +0600

    Update c4100010101.lua

--- script/c4100010101.lua
+++ script/c4100010101.lua
@@ -2,7 +2,7 @@
 Duel.LoadScript("duellinks.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	DuelLinks.AddProcedure(c,nil,s.flipop,1)
+	DuelLinks.AddProcedure(c,true,s.flipop,1)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
