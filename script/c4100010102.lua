a970ea0000b85a3c3dfd19b4d7d244c458d3f97a
Almatik <almatiktk@gmail.com>
2022-10-16 23:46:51 +0600

    Update c4100010102.lua

--- script/c4100010102.lua
+++ script/c4100010102.lua
@@ -4,35 +4,21 @@ local s,id=GetID()
 function s.initial_effect(c)
 	DuelLinks.AddProcedure(c,nil,s.flipop)
 	DuelLinks.Trigger(c,nil,s.flipop,1,EVENT_PHASE+PHASE_END)
-	aux.GlobalCheck(s,function()
-		s[0]=true
-		s[1]=true
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_SUMMON_SUCCESS)
-		ge1:SetOperation(s.checkop)
-		Duel.RegisterEffect(ge1,0)
-		local ge2=Effect.CreateEffect(c)
-		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge2:SetCode(EVENT_ADJUST)
-		ge2:SetCountLimit(1)
-		ge2:SetOperation(s.clear)
-		Duel.RegisterEffect(ge2,0)
-	end)
+	local se1=Effect.CreateEffect(c)
+	se1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	se1:SetCode(EVENT_SUMMON_SUCCESS)
+	se1:SetOperation(s.checkop)
+	c:RegisterEffect(se1)
 end
 function s.checkop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=eg:GetFirst()
 	if tc:IsCode(43096270) then
-		s[tc:GetControler()]=true
+		Duel.RegisterFlagEffect(ep,id,0,0,0)
 	end
-end
-function s.clear(e,tp,eg,ep,ev,re,r,rp)
-	s[0]=false
-	s[1]=false
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,tp)
 	return DuelLinks.IsEndPhase() and DuelLinks.IsTurnPlayer(tp)
-		and s[tp]
+		and Duel.GetFlagEffect(ep,id)~=0
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	DuelLinks.FlipUp(e:GetHandler())
