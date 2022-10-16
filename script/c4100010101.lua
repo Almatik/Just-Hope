02c8714f924dd76399b0481bc239d5bd2bf83b93
Almatik <almatiktk@gmail.com>
2022-10-16 23:03:40 +0600

    f

--- script/c4100010101.lua
+++ script/c4100010101.lua
@@ -4,7 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	DuelLinks.AddProcedure(c,nil,s.flipop)
 	DuelLinks.PreStart(c,nil,s.flipop,1)
-	DuelLinks.Predraw(c,nil,s.flipop,1)
+	--DuelLinks.Predraw(c,nil,s.flipop,1)
 	DuelLinks.Ignition(c,nil,s.flipop,1)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
--- script/duellinks.lua
+++ script/duellinks.lua
@@ -33,7 +33,8 @@ function DuelLinks.PreStart(c,skillcon,skillop,countlimit)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_STARTUP)
 	e1:SetRange(0x5f)
-	e1:SetOperation(DuelLinks.SkillOp(skillcon,skillop,countlimit,EVENT_STARTUP))
+	e1:SetOperation(DuelLinks.SkillOp(skillcon,skillop,countlimit,EVENT_PREDRAW),
+					DuelLinks.SkillOp(skillcon,skillop,countlimit,EVENT_STARTUP))
 	c:RegisterEffect(e1)
 end
 function DuelLinks.Predraw(c,skillcon,skillop,countlimit)
