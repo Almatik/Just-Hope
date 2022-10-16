2d968ab3cb2610ae3428fceb3bd44ca487dcdbc8
Almatik <almatiktk@gmail.com>
2022-10-16 23:32:48 +0600

    Update c4100010102.lua

--- script/c4100010102.lua
+++ script/c4100010102.lua
@@ -10,8 +10,6 @@ function s.flipcon(e,tp,eg,ep,ev,re,r,tp)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	DuelLinks.FlipUp(e:GetHandler())
-	for tp=0,1 do
-		local num=6-Duel.GetFieldGroupCount(tp,LOCATION_HAND)
-		Duel.Draw(tp,num,REASON_RULE)
-	end
+	local num=6-Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)
+	Duel.Draw(tp,num,REASON_RULE)
 end
