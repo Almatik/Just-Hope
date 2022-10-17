c9511d97c06e4d9ab408c09d9cc42ab7344830ef
Almatik <almatiktk@gmail.com>
2022-10-17 15:34:40 +0600

    Update c4100010102.lua

--- script/c4100010102.lua
+++ script/c4100010102.lua
@@ -3,13 +3,23 @@ Duel.LoadScript("duellinks.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	DuelLinks.AddProcedure(c,nil,s.flipop)
+	DuelLinks.Trigger(c,s.summcon,s.summop,1,EVENT_SUMMON_SUCCESS)
 	DuelLinks.Trigger(c,s.flipcon,s.flipop,1,EVENT_PHASE+PHASE_END)
 end
-function s.filter(c)
-	return :IsSummonType(SUMMON_TYPE_NORMAL) and c:IsStatus(STATUS_SUMMON_TURN)
-end
 function s.flipcon(e,tp,eg,ep,ev,re,r,tp)
-	return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil)
+	local tc=eg:GetFirst()
+	while tc do
+		if tc:IsCode(43096270) then
+			return true
+		end
+	end
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.RegisterFlagEffect(tp,id,0,0,0)
+end
+
+function s.flipcon(e,tp,eg,ep,ev,re,r,tp)
+	return Duel.GetFlagEffect(tp,id)~=0
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	DuelLinks.FlipUp(e:GetHandler())
