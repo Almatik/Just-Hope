57fb7e5a72bb5dc712c6e069e0a35cc29539c278
Almatik <almatiktk@gmail.com>
2022-10-17 15:37:23 +0600

    Update c4100010102.lua

--- script/c4100010102.lua
+++ script/c4100010102.lua
@@ -3,20 +3,17 @@ Duel.LoadScript("duellinks.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	DuelLinks.AddProcedure(c,nil,s.flipop)
-	DuelLinks.Trigger(c,s.summcon,s.summop,1,EVENT_SUMMON_SUCCESS)
+	DuelLinks.Trigger(c,nil,s.summop,1,EVENT_SUMMON_SUCCESS)
 	DuelLinks.Trigger(c,s.flipcon,s.flipop,1,EVENT_PHASE+PHASE_END)
 end
-function s.summcon(e,tp,eg,ep,ev,re,r,tp)
+function s.summop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=eg:GetFirst()
 	while tc do
 		if tc:IsCode(43096270) then
-			return true
+			Duel.RegisterFlagEffect(tp,id,0,0,0)
 		end
 		tc=eg:GetNext()
 	end
-end
-function s.summop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.RegisterFlagEffect(tp,id,0,0,0)
 end
 
 function s.flipcon(e,tp,eg,ep,ev,re,r,tp)
