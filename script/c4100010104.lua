373fa1e1c4d37c85d7b3f7086a0f8702b62e1a89
Almatik <almatiktk@gmail.com>
2022-10-20 21:40:39 +0600

    Update c4100010104.lua

--- script/c4100010104.lua
+++ script/c4100010104.lua
@@ -13,7 +13,7 @@ function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentChain()==0 and tp==Duel.GetTurnPlayer() and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0
 		and Duel.GetDrawCount(tp)>0
 		and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_DECK,0,1,nil,83764718)
-		and (Duel.GetLP(tp)<=2500 or Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil))
+		and (Duel.GetLP(tp)<=2000 or Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil))
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--ask if you want to activate the skill or not
