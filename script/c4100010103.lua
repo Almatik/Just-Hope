fa7b4640ada4b51e7575d4e06c9d8de9433924f3
Almatik <almatiktk@gmail.com>
2022-10-20 21:33:40 +0600

    Update c4100010103.lua

--- script/c4100010103.lua
+++ script/c4100010103.lua
@@ -4,26 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	DuelLinks.AddProcedure(c)
 	DuelLinks.Predraw(c,s.flipcon,s.flipop,1)
-	aux.GlobalCheck(s,function()
-		s[0]=nil
-		s[1]=nil
-		s[2]=0
-		s[3]=0
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_ADJUST)
-		ge1:SetOperation(s.checkop)
-		Duel.RegisterEffect(ge1,0)
-	end)
-end
-function s.checkop()
-	for tp=0,1 do
-		if not s[tp] then s[tp]=Duel.GetLP(tp) end
-		if s[tp]>Duel.GetLP(tp) then
-			s[2+tp]=s[2+tp]+(s[tp]-Duel.GetLP(tp))
-			s[tp]=Duel.GetLP(tp)
-		end
-	end
+	DuelLinks.CheckLP(s,c)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--condition
