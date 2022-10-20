4ac5f4cfc9b1836d76776e2201a0f27f9f0baa40
Almatik <almatiktk@gmail.com>
2022-10-20 23:35:28 +0600

    Update c3900010106.lua

--- script/c3900010106.lua
+++ script/c3900010106.lua
@@ -37,17 +37,17 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	local mg=Group.CreateGroup()
+	mg:Merge(c)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp,c)
 	local tc=g:GetFirst()
 	local tm=tc:GetMetatable(true)
 	if not tm then return false end
 	local bn=tm.bleach_name
+	mg:Merge(tc)
 	if tc then
 		local tl=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,bn):GetFirst()
-		local mg=Group.CreateGroup()
-		mg:Merge(c)
-		mg:Merge(tc)
 		tl:SetMaterial(mg)
 		Duel.SendtoGrave(mg,REASON_EFFECT+REASON_MATERIAL+REASON_LINK)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
