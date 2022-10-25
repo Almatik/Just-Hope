bdaccd739c6019821d0b3ab572a8f5b7148e6570
Almatik <almatiktk@gmail.com>
2022-10-25 18:03:27 +0600

    Update c3900010211.lua

--- script/c3900010211.lua
+++ script/c3900010211.lua
@@ -67,7 +67,9 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 			e1:SetValue(1000)
 			c:RegisterEffect(e1)
 	end
 	Duel.SpecialSummonComplete()
+end
